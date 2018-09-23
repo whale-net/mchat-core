d0c750be6f48dafdcb61196c80c35b11bf00c003
Alex Harding <cpsaicontact@gmail.com>
2018-09-23 00:38:15 +0000

    Database Schema complete

--- sql/schema.sql
+++ sql/schema.sql
@@ -7,17 +7,7 @@ created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
 password CHAR(255) NOT NULL, /* alg+salt+hash = ~90-150b */
 PRIMARY KEY(uid),
 UNIQUE(username)
-)
-
-CREATE TABLE Groups-Users (
-    uid INTEGER,
-    gid INTEGER,
-    display_name VARCHAR(64) NOT NULL,
-    avatar CHAR(64),
-    PRIMARY KEY (uid, gid),
-    FOREIGN KEY (uid) REFERENCES Users(uid),
-    FOREIGN KEY (gid) REFERENCES Groups(gid)
-)
+);
 
 CREATE TABLE Groups (
     gid INTEGER AUTO_INCREMENT,
@@ -25,8 +15,26 @@ CREATE TABLE Groups (
     owner_id INTEGER NOT NULL,
     name VARCHAR(128),
     PRIMARY KEY (gid),
-    FOREIGN KEY (creator_id)
-)
+    FOREIGN KEY (creator_id) REFERENCES Users(uid)
+);
+
+CREATE TABLE Groups_Users (
+    uid INTEGER,
+    gid INTEGER,
+    display_name VARCHAR(64) NOT NULL,
+    avatar CHAR(64),
+    PRIMARY KEY (uid, gid),
+    FOREIGN KEY (uid) REFERENCES Users(uid),
+    FOREIGN KEY (gid) REFERENCES Groups(gid)
+);
+
+/* when confused return to this https://stackoverflow.com/questions/2366854/can-table-columns-with-a-foreign-key-be-null */
+CREATE TABLE Images (
+    hash CHAR(64),
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    PRIMARY KEY (hash)
+);
+
 
 /* TODO make it so a blank message is possible with an image. triggers? SIGNAL! */
 CREATE TABLE Messages (
@@ -39,11 +47,6 @@ CREATE TABLE Messages (
     img_hash CHAR(64),
     PRIMARY KEY (MID),
     FOREIGN KEY (img_hash) REFERENCES Images(hash)
-)
+);
 
-/* when confused return to this https://stackoverflow.com/questions/2366854/can-table-columns-with-a-foreign-key-be-null */
-CREATE TABLE Images (
-    hash CHAR(64)
-    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    PRIMARY KEY (hash)
-)
+
