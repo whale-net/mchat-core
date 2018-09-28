2e6a414a11b59ad083a5f9034ead5ef7a33c7272
alhardng <alhardng@umich.edu>
2018-09-28 15:30:30 -0400

    import from envar added

--- chat/__init__.py
+++ chat/__init__.py
@@ -7,8 +7,10 @@ import flask
 
 mchat = flask.Flask(__name__) # pylint: disable=invalid-name
 
-# Load config
+# Load config from file
 mchat.config.from_object('chat.config')
+# Load config from environment variable
+mchat.config.from_envar('CHAT_SETTINGS', silent=True)
 
 # give chat app access to api model and friends
 import chat.api # pylint: disable=wrong-import-position
