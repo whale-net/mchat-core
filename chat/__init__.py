f9aebd1766cfbc7c700b06fb9b39442b744808d4
alhardng <alhardng@umich.edu>
2018-09-28 14:47:04 -0400

    app name changed to mchat

--- chat/__init__.py
+++ chat/__init__.py
@@ -5,10 +5,10 @@ Alex Harding <alex.harding@whale-net.net>
 """
 import flask
 
-chat = flask.Flask(__name__) # pylint: disable=invalid-name
+mchat = flask.Flask(__name__) # pylint: disable=invalid-name
 
 # Load config
-chat.config.from_object('chat.config')
+mchat.config.from_object('chat.config')
 
 # give chat app access to api model and friends
 import chat.api # pylint: disable=wrong-import-position
