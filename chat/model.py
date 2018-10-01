cb866b51ff2bf83506e8c0360f19587859cfd237
Alex Harding <alex.harding@whale-net.net>
2018-10-01 18:31:25 -0400

    began adding db functions

--- chat/model.py
+++ chat/model.py
@@ -5,6 +5,26 @@ import mysql.connector
 import flask
 import chat
 
+from util.password import *
+
+def create_user(username, display_name, avatar='', password):
+    """
+    Create a new user.
+    """
+    return ""
+
+def verify_username_password(username, password):
+    """
+    Verify a user's password given their username.
+    """
+    cursor = get_db().cursor()
+    cursor.execute(
+        "SELECT password FROM Users WHERE username=?", (username,))
+    )
+    db_password_string = cursor.fetchone()['password']
+    return verify_username_password(password, db_password_string)
+
+
 def get_db():
     """
     Return database connection or open new one if not present.
